05d6ee7a06d35342954adab440d75221133d9230
Melanie Zuk <zenani3@gmail.com>
2018-07-03 15:43:33 -0400

    updated file

---
+++ keys.js
@@ -0,0 +1,16 @@
+//omdb api key http://www.omdbapi.com/?i=tt3896198&apikey=1bee6a22
+
+
+console.log('this is loaded');
+
+exports.twitter = {
+  consumer_key: BHEANQiCh5cSpHpFIESFLYSXO
+  consumer_secret: ,
+  access_token_key: ,
+  access_token_secret:
+};
+
+exports.spotify = {
+  id: 3ebf2acee5e4476096b55400f28f826e
+  secret: 42611ae79e26483bb9daceaf7ad54141 
+};
