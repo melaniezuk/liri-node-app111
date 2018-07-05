1c3b6ec5be2aace063f7746ae06579b61ac7a256
Melanie Zuk <zenani3@gmail.com>
2018-07-05 17:57:42 -0400

    revision

--- keys.js
+++ keys.js
@@ -3,10 +3,10 @@
 
 console.log('this is loaded');
 
-exports.twitter = {
+module.exports = {
   consumer_key: hBwb960yVaPLz7JZc0reKlpG5
   consumer_secret: ddBDuQOeJ7eE48JS2H7rvMMkXhUXVwOuTiaYeDswQoIKIb3GcS
-    access_token_key: 1014714406935154689-ai2PWlbfANUaftIzCB6ly8AQsLLIC5 
+  access_token_key: 1014714406935154689-ai2PWlbfANUaftIzCB6ly8AQsLLIC5 
   access_token_secret: K2SRE8OZZwnVSkFkCGSrn3fneeSeUffrJ5NLRSFSrFfVM
 };
 
