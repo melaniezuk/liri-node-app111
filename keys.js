8cdf17f272a3a0155e05a8070db2b801facc7ec6
Melanie Zuk <zenani3@gmail.com>
2018-07-09 14:57:26 -0400

    got twitter working

--- keys.js
+++ keys.js
@@ -3,14 +3,18 @@
 
 console.log('this is loaded');
 
-module.exports = {
-  consumer_key: hBwb960yVaPLz7JZc0reKlpG5
-  consumer_secret: ddBDuQOeJ7eE48JS2H7rvMMkXhUXVwOuTiaYeDswQoIKIb3GcS
-  access_token_key: 1014714406935154689-ai2PWlbfANUaftIzCB6ly8AQsLLIC5 
-  access_token_secret: K2SRE8OZZwnVSkFkCGSrn3fneeSeUffrJ5NLRSFSrFfVM
+exports.twitter = {
+   consumer_key: process.env.TWITTER_CONSUMER_KEY,
+   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+   access_token: process.env.TWITTER_ACCESS_TOKEN_KEY,
+   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 };
 
-exports.spotify = {
-  id: 3ebf2acee5e4476096b55400f28f826e
-  secret: 42611ae79e26483bb9daceaf7ad54141 
-};
+
+  
+  
+  /*exports.spotify = {
+  id: '3ebf2acee5e4476096b55400f28f826e',
+  secret: '42611ae79e26483bb9daceaf7ad54141',
+};*/
+
